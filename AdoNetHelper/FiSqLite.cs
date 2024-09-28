9e4848361a31c72b9fc633d086a2619e89f8b212
Tuncay_hp <tunc270@gmail.com>
2024-09-29 01:06:20 +0300

    up - sqlite

---
+++ AdoNetHelper/FiSqLite.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace OrakYazilimLib.AdoNetHelper
+{
+	public partial class FiSqLite
+	{
+		public string ConnString { get; private set; }
+		public SQLiteConnection Conn { get; private set; }
+		
+		/// <summary>
+		/// Comm : Command
+		/// </summary>
+		//public SQLiteCommand Comm { get; private set; }
+
+		//public bool boConn { get; set; }
+
+
+		public FiSqLite(string _connString)
+		{
+			//ConnString = _connString;
+			//Conn = new SqlConnection(ConnString);
+			//Comm = Conn.CreateCommand();
+			//SQLiteConnection sqlite_conn;
+			// Create a new database connection:
+			//string connstr = @"Data Source=Y:\sqlite-data\demo.db";
+			this.ConnString = _connString;
+			this.Conn = new SQLiteConnection(_connString);
+			
+			// Open the connection:
+			try
+			{
+				this.Conn.Open();
+				Console.WriteLine("connected");
+				//this.boConn=true;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				//this.boConn = false;
+			}
+			//return sqlite_conn;
+		}
+
+
+		private SQLiteParameter[] ProcessParameters(params ParamItem[] parameters)
+		{
+			//SqlParameter[] pars = parameters.Select(x => new SqlParameter()
+			//{
+			//	ParameterName = x.ParamName,
+			//	Value = x.ParamValue
+			//}).ToArray();
+
+			//return pars;
+			return null;
+		}
+
+
+		public virtual int RunQuery(string query, params ParamItem[] parameters)
+		{
+			//Comm.Parameters.Clear();
+			//Comm.CommandText = query;
+			//Comm.CommandType = CommandType.Text;
+
+			//if (parameters != null && parameters.Length > 0)
+			//{
+			//	Comm.Parameters.AddRange(ProcessParameters(parameters));
+			//}
+
+			//int result = 0;
+
+			//Conn.Open();
+			//try
+			//{
+			//	result = Comm.ExecuteNonQuery();
+			//	if (result == -1) result = 1;
+			//}
+			//catch (Exception e)
+			//{
+			//	Console.WriteLine(e);
+			//	result = -2;
+			//	//throw;
+			//}
+
+			//Conn.Close();
+
+			//return result;
+			return 0;
+		}
+
+
+		public virtual DataTable RunProc(string procName, params ParamItem[] parameters)
+		{
+			//Comm.Parameters.Clear();
+			//Comm.CommandText = procName;
+			//Comm.CommandType = CommandType.StoredProcedure;
+
+			//if (parameters != null && parameters.Length > 0)
+			//{
+			//	Comm.Parameters.AddRange(ProcessParameters(parameters));
+			//}
+
+			//DataTable dt = new DataTable();
+			//SqlDataAdapter adapter = new SqlDataAdapter(Comm);
+			//adapter.Fill(dt);
+
+			//return dt;
+			return null;
+		}
+
+
+		public virtual DataTable GetTable(string query, params ParamItem[] parameters)
+		{
+			//Comm.Parameters.Clear();
+			//Comm.CommandText = query;
+			//Comm.CommandType = CommandType.Text;
+
+			//if (parameters != null && parameters.Length > 0)
+			//{
+			//	Comm.Parameters.AddRange(ProcessParameters(parameters));
+			//}
+
+			//SqlDataAdapter da = new SqlDataAdapter(Comm);
+
+			//// Adaptor : otomatik bağlantı açar. Verileri çeker(sorguyu çalıştırır) ve bir datatable 'a doldurur ve bağlantıyı otomatik kapatır.
+
+			//DataTable dt = new DataTable();
+			//da.Fill(dt);
+
+			//return dt;
+
+			//SQLiteDataReader sqlite_datareader;
+			SQLiteCommand sqlite_cmd;
+			sqlite_cmd = this.Conn.CreateCommand();
+			sqlite_cmd.CommandText = query; // sqlite_cmd.CommandText = "SELECT * FROM test1";
+
+			SQLiteDataAdapter da = new SQLiteDataAdapter(sqlite_cmd);
+
+			// Adaptor : otomatik bağlantı açar. Verileri çeker(sorguyu çalıştırır) ve bir datatable 'a doldurur ve bağlantıyı otomatik kapatır.
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+
+			return dt;
+		}
+	}
+}
--- Util/core/FiDataTable.cs
+++ Util/core/FiDataTable.cs
@@ -1,28 +1,43 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
 
 namespace OrakYazilimLib.Util.core
 {
-    public class FiDataTable
-    {
-        public static List<string> convertListString(DataTable dataTable,string txFieldName)
-        {
-            List<string> list = new List<string>();
-            
-            foreach (DataRow dataTableRow in dataTable.Rows)
-            {
-                if (dataTableRow[txFieldName]==null)
-                {
-                    continue;
-                }
+	public class FiDataTable
+	{
+		public static List<string> convertListString(DataTable dataTable, string txFieldName)
+		{
+			List<string> list = new List<string>();
 
-                list.Add(dataTableRow[txFieldName].ToString());
-            }
-            
-            // Linq ile yapılışı
-            // (from DataRow dataTableRow in dataTable.Rows where dataTableRow[txFieldName] != null select dataTableRow[txFieldName].ToString()).ToList();
+			foreach (DataRow dataTableRow in dataTable.Rows)
+			{
+				if (dataTableRow[txFieldName] == null)
+				{
+					continue;
+				}
 
-            return list;
-        }
-    }
+				list.Add(dataTableRow[txFieldName].ToString());
+			}
+
+			// Linq ile yapılışı
+			// (from DataRow dataTableRow in dataTable.Rows where dataTableRow[txFieldName] != null select dataTableRow[txFieldName].ToString()).ToList();
+
+			return list;
+		}
+
+
+		public static void printDt(DataTable dataTable)
+		{
+			foreach (DataRow dr in dataTable.Rows)
+			{
+				foreach (var item in dr.ItemArray)
+				{
+					Console.Write(item + " ");
+				}
+				Console.WriteLine();
+			}
+
+		}
+	}
 }
