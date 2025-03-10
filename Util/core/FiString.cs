3e3ea4c547797ae7907dc4fd0d01ffbd772230b0
Tuncay <tunc270@gmail.com>
2025-03-10 17:25:21 +0300

    up

--- Util/core/FiString.cs
+++ Util/core/FiString.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -46,6 +47,33 @@ namespace OrakYazilimLib.Util
         {
             return txValue == null || IsEmpty(txValue.Trim());
         }
+
+        public static string ToUpperCamelCase(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            // Büyük harfleri algılamak ve her birini parçalara ayırmak için bir algoritma.
+            var words = new System.Text.StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                // İlk harf veya bir harften sonra gelen büyük harf yeni bir kelime olarak kabul edilir.
+                if (i == 0 || char.IsUpper(input[i]) && (i > 0 && char.IsLower(input[i - 1])))
+                {
+                    if (words.Length > 0)
+                        words.Append(" "); // Kelimeleri ayırmak için boşluk ekle.
+                }
+
+                words.Append(char.ToUpper(input[i]));
+            }
+
+            // Her parçanın ilk harfini büyük yap ve birleştir.
+            var textInfo = CultureInfo.InvariantCulture.TextInfo;
+            return textInfo.ToTitleCase(words.ToString().ToLower()).Replace(" ", string.Empty);
+        }
+
 
 
     }
