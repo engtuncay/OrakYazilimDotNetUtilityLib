43383e45a3b07335927e063eb84c001c33087a49
TuncayHp <tunc270@gmail.com>
2025-03-12 09:48:34 +0300

    up

---
+++ DataContainer/DtoFiCol.cs
@@ -0,0 +1,34 @@
+﻿using OrakYazilimLib.DbGeneric;
+using OrakYazilimLib.Util.Collection;
+using System.Collections.Generic;
+
+namespace OrakYazilimLib.DataContainer
+{
+  public class DtoFiCol
+  {
+    public static List<dynamic> ImportDtoFiCol2(FiColList fiColList)
+    {
+      List<dynamic> list = new List<dynamic>();
+
+      foreach (var ficol in fiColList)
+      {
+        dynamic obj = CreateDynamicFiCol2(ficol);
+        list.Add(obj);
+      }
+
+      return list;
+    }
+    public static dynamic CreateDynamicFiCol2(FiCol ficol)
+    {
+      dynamic obj = new System.Dynamic.ExpandoObject();
+
+      obj.ofcTxFieldName = ficol.ofcTxFieldName;
+      obj.ofcTxHeader = ficol.ofcTxHeader;
+      obj.ofcLength = ficol.ofcLnLength;
+      obj.ofcLnPrecision = ficol.ofcLnPrecision;
+      obj.ofcTxFieldDesc = ficol.ofcTxFieldDesc;
+
+      return obj;
+    }
+  }
+}
