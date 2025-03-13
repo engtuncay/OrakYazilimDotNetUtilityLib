bedc7b10a442ea4545839b3c2edecaf59afd7f9a
Tuncay <tunc270@gmail.com>
2025-03-13 17:01:34 +0300

    up

--- DbGeneric/FiCol.cs
+++ DbGeneric/FiCol.cs
@@ -3,62 +3,66 @@
 namespace OrakYazilimLib.DbGeneric
 {
 
-    public class FiCol
+  public class FiCol
+  {
+    public string ofcTxFieldName { get; set; }
+    public string ofcTxHeader { get; set; }
+
+    /**
+     * Alanın Veri Türü (FiColType dan alınabilir)
+     */
+    public string ofcTxColType { get; set; }
+
+    public bool? ofcBoTransient { get; set; }
+
+    public bool? ofcBoNullable { get; set; }
+
+    public string ofcTxFieldDesc { get; set; }
+
+    public string ofcTxFieldType { get; set; }
+
+    public int? ofcLnLength { get; set; }
+    public int? ofcLnPrecision { get; set; }
+    public int? ofcLnScale { get; set; }
+
+    public string ofcTxDbField { get; set; }
+
+    /**
+     * Alanın varsayılan veri türü
+     *
+     */
+    public FiColType fiColType { get; set; }
+
+    public FiCol(string ofcTxFieldName)
     {
-        public string ofcTxFieldName { get; set; }
-        public string ofcTxHeader { get; set; }
-
-        /**
-         * Alanın Veri Türü (FiColType dan alınabilir)
-         */
-        public string ofcTxColType { get; set; }
-
-        public bool? ofcBoTransient { get; set; }
-
-        public string ofcTxFieldDesc { get; set; }
-
-        public string ofcTxFieldType { get; set; }
-
-        public int? ofcLnLength { get; set; }
-        public int? ofcLnPrecision { get; set; }
-        public int? ofcLnScale { get; set; }
-
-        /**
-         * Alanın varsayılan veri türü
-         *
-         */
-        public FiColType fiColType { get; set; }
-
-        public FiCol(string ofcTxFieldName)
-        {
-            this.ofcTxFieldName = ofcTxFieldName;
-        }
-
-        public FiCol(string ofcTxFieldName, string ofcTxHeader)
-        {
-            this.ofcTxFieldName = ofcTxFieldName;
-            this.ofcTxHeader = ofcTxHeader;
-        }
-
-        public string GetFieldName()
-        {
-            return this.ofcTxFieldName;
-        }
-
-        public FiCol()
-        {
-        }
-
-        public override string ToString()
-        {
-            return this.ofcTxFieldName ?? "";
-        }
-        
-        public FiCol BuiColType(FiColType fiColType)
-        {
-            this.fiColType = fiColType;
-            return this;
-        }
+      this.ofcTxFieldName = ofcTxFieldName;
     }
+
+    public FiCol(string ofcTxFieldName, string ofcTxHeader)
+    {
+      this.ofcTxFieldName = ofcTxFieldName;
+      this.ofcTxHeader = ofcTxHeader;
+    }
+
+    public string GetFieldName()
+    {
+      return this.ofcTxFieldName;
+    }
+
+    public FiCol()
+    {
+    }
+
+    public override string ToString()
+    {
+      return this.ofcTxFieldName ?? "";
+    }
+
+    public FiCol BuiColType(FiColType fiColType)
+    {
+      this.fiColType = fiColType;
+      return this;
+    }
+  }
 
 }
