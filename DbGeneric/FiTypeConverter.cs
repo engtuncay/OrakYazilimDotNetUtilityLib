5050ac56d25361d6831770fa70dd553d49fdaef6
Tuncay <tunc270@gmail.com>
2025-08-26 15:13:00 +0300

    up and rfc

---
+++ DbGeneric/FiTypeConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace OrakYazilimLib.DbGeneric
+{
+  public static class FiTypeConverter
+  {
+
+
+
+
+    public static object ConvertByFiCol(FiCol fiCol, string txValue)
+    {
+      //fiCol.ofcTxFieldType
+      return txValue;
+    }
+  }
+}
