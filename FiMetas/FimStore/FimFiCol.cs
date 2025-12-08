27762b5ed4f3887cd8e310fd49637fc0316f6484
TuncayHp2 <tunc270@gmail.com>
2025-12-08 16:08:45 +0300

    Add FkcEnwUser class and related files; update project structure

---
+++ FiMetas/FimStore/FimFiCol.cs
@@ -0,0 +1,147 @@
+﻿namespace OrakYazilimLib.FiMetas.FimStore
+{
+  // Codegen v2
+  using OrakYazilimLib.Util.core;
+
+  public class FimFiCol
+  {
+
+    public static FiMeta OfcTxDesc()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxDesc");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxPrefix()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxPrefix");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxEntityName()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxEntityName");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxFieldName()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxFieldName");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxFieldType()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxFieldType");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxHeader()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxHeader");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxDbField()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxDbField");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxRefField()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxRefField");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcLnLength()
+    {
+      FiMeta fiMeta = new FiMeta("ofcLnLength");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcLnPrecision()
+    {
+      FiMeta fiMeta = new FiMeta("ofcLnPrecision");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcLnScale()
+    {
+      FiMeta fiMeta = new FiMeta("ofcLnScale");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcBoNullable()
+    {
+      FiMeta fiMeta = new FiMeta("ofcBoNullable");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxIdType()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxIdType");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcBoTransient()
+    {
+      FiMeta fiMeta = new FiMeta("ofcBoTransient");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcBoUnique()
+    {
+      FiMeta fiMeta = new FiMeta("ofcBoUnique");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcBoUniqGro1()
+    {
+      FiMeta fiMeta = new FiMeta("ofcBoUniqGro1");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfcTxDefValue()
+    {
+      FiMeta fiMeta = new FiMeta("ofcTxDefValue");
+
+      return fiMeta;
+    }
+
+
+  }
+
+}
---
+++ FiMetas/FimStore/FimFiMeta.cs
@@ -0,0 +1,42 @@
+﻿using OrakYazilimLib.Util.core;
+
+namespace OrakYazilimLib.FiMetas.FimStore
+{
+  // Codegen v2
+
+  public class FimFiMeta
+  {
+
+    public static FiMeta OfmTxKey()
+    {
+      FiMeta fiMeta = new FiMeta("ofmTxKey");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfmTxValue()
+    {
+      FiMeta fiMeta = new FiMeta("ofmTxValue");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfmTxLabel()
+    {
+      FiMeta fiMeta = new FiMeta("ofmTxLabel");
+
+      return fiMeta;
+    }
+
+
+    public static FiMeta OfmLnKey()
+    {
+      FiMeta fiMeta = new FiMeta("ofmLnKey");
+
+      return fiMeta;
+    }
+  }
+
+}
